c64d5432ee003e939f4fbc49503fe24db687645c
Felipe Augusto <felipe.aga@gmail.com>
2015-09-25 21:07:34 -0300

    Adicionado crash listener.

--- app/src/main/java/riomaissaude/felipe/com/br/riomaissaude/crashlistener/ApplicationCrashListener.java
+++ app/src/main/java/riomaissaude/felipe/com/br/riomaissaude/crashlistener/ApplicationCrashListener.java
@@ -4,17 +4,16 @@ import org.acra.ACRA;
 import org.acra.ReportingInteractionMode;
 import org.acra.annotation.ReportsCrashes;
 
-import vizbom.util.CrashSender;
 import android.app.Application;
 
 @ReportsCrashes(formKey = "", mode = ReportingInteractionMode.SILENT)
 public class ApplicationCrashListener extends Application {
 
-	@Override
-	public void onCreate() {
-		ACRA.init(this);
-		CrashSender crashSender = new CrashSender();
-		ACRA.getErrorReporter().setReportSender(crashSender);
-		super.onCreate();
-	}
+    @Override
+    public void onCreate() {
+        ACRA.init(this);
+        CrashSender crashSender = new CrashSender();
+        ACRA.getErrorReporter().setReportSender(crashSender);
+        super.onCreate();
+    }
 }
--- app/src/main/java/riomaissaude/felipe/com/br/riomaissaude/utils/WebService.java
+++ app/src/main/java/riomaissaude/felipe/com/br/riomaissaude/utils/WebService.java
@@ -5,7 +5,7 @@ package riomaissaude.felipe.com.br.riomaissaude.utils;
  */
 public class WebService {
 
-    public static final String ENDERECO_WS = "http://192.168.0.103:8080/RioSaudeWS/";
+    public static final String ENDERECO_WS = "http://192.168.0.104:8080/RioSaudeWS/";
 
     public static final String RESPOSTA_SUCESSO = "sucesso";
     public static final String RESPOSTA_ERRO = "erro";
